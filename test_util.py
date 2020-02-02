6a855e5944bed773e9c9fd2e2e46c2dc6fc49f67
Alistair John Madden <ajm@houndson.io>
2020-02-02 13:12:24 +0000

    Add tests for reading sudoku from file

---
+++ test_util.py
@@ -0,0 +1,110 @@
+import json
+import unittest
+
+import util
+
+
+class TestReadSudokuFromFile(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_valid_file(self):
+        expected_output = {
+            "sudokus": [
+                [
+                    8,
+                    None,
+                    None,
+                    9,
+                    3,
+                    None,
+                    None,
+                    None,
+                    2,
+                    None,
+                    None,
+                    9,
+                    None,
+                    None,
+                    None,
+                    None,
+                    4,
+                    None,
+                    7,
+                    None,
+                    2,
+                    1,
+                    None,
+                    None,
+                    9,
+                    6,
+                    None,
+                    2,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    9,
+                    None,
+                    None,
+                    6,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                    7,
+                    None,
+                    None,
+                    7,
+                    None,
+                    None,
+                    None,
+                    6,
+                    None,
+                    None,
+                    5,
+                    None,
+                    2,
+                    7,
+                    None,
+                    None,
+                    8,
+                    4,
+                    None,
+                    6,
+                    None,
+                    3,
+                    None,
+                    None,
+                    None,
+                    None,
+                    5,
+                    None,
+                    None,
+                    5,
+                    None,
+                    None,
+                    None,
+                    6,
+                    2,
+                    None,
+                    None,
+                    8
+                ]
+            ]
+        }
+
+        actual_output = util.read_sudoku_from_file('sudokus_test.json')
+
+        self.assertEqual(actual_output, expected_output)
+
+    def test_invalid_file(self):
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            util.read_sudoku_from_file(__file__)
+
+    def test_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            util.read_sudoku_from_file('notafile')
--- util.py
+++ util.py
@@ -2,10 +2,7 @@ import json
 
 
 def read_sudoku_from_file(file):
-    try:
-        with open(file) as file_handle:
-            sudoku_data = json.load(file_handle)
-    except FileNotFoundError:
-        sudoku_data = {'sudokus': []}
+    with open(file) as file_handle:
+        sudoku_data = json.load(file_handle)
 
     return sudoku_data
